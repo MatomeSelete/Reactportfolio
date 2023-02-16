6854dbcf09e3b5edb2056a1c644c0af60710c6d7
DA_M1_35 <da_m1_35@Academy-learners-MacBook-Air.local>
2023-02-16 10:59:51 +0200

    changes

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -22,7 +22,7 @@ function Navigation() {
           <Link to="/about.js">About</Link>
         </p>
         <p className='navP'>
-          <Link to="/resume.js">ResumePage</Link>
+          <Link to="/resume.js">Resume</Link>
         </p>
       </div>
 
